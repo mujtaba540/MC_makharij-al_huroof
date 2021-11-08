4243b8ee6acdf0bca633d91d0c94fe77af959f5c
mujtaba <mujtaba@salesassist.pk>
2021-11-08 18:58:06 +0500

    alyout created

--- app/src/main/java/com/example/makharij_al_huroof/MainActivity.java
+++ app/src/main/java/com/example/makharij_al_huroof/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.makharij_al_huroof;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button app,repo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        repo=findViewById(R.id.repo);
+        app=findViewById(R.id.app);
+
+        repo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url="https://github.com/mujtaba540/MC_makharij-al_huroof";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Home.class);
+                startActivity(i);
+            }
+        });
     }
 }
